9b8aa9fe3cc1b4e2f6ab713bba31ac1e781c39cd
Felipe <f.tommaselli@usp.br>
2021-09-07 21:59:26 -0300

    Melhorias no metodo das secantes

--- Trabalho1/secantes.py
+++ Trabalho1/secantes.py
@@ -10,43 +10,49 @@ def main():
     
     maxiter = input("Insira um valor para o MAXITER que você deseja:\n")
 
+    #* INTERVALO [-1,0]
+
     k = 0 # começando com k igual a 0 e incrementando a cada iteração
-    xk = -1 # tendo o primeiro intervalo de [-1, 0] e estabelecendo xk = -1 e xr = 0
-    xr = 0 # xr, sendo r = k - 1 
+    xk = -1. # tendo o primeiro intervalo de [-1, 0] e estabelecendo xk = -1 e xr = 0
+    xr = 0. # xr, sendo r = k - 1 
     xrr = None # xrr, sendo rr = r - 1 = k - 2
-    mod = abs(xk - xr)
     raiz = -pow(3, 0.5)/3.
 
-    arquivo.write("No intervalo [-1, 0]\n")
+    # cabeçalho do arquivo texto
+    arquivo.write("No intervalo [-1,0]\n")
     arquivo.write("\tk\t     xk\t\t   f(xk)\t   erro\n")
 
     # enquanto não for alcançada a precisão desejada e 
     # k for menor que a maxiter o loop acontece
-    while(mod > 0.000001 and k <= int(maxiter)):
-        
-        print(k)
-        fxk = funcao(xk)
-        fxr = funcao(xr)
+    while(abs(xk - xr) > 0.000001 and k <= int(maxiter)):
+        k, xk, xr, xrr, arquivo = m_das_secantes(k, xk, xr, xrr, arquivo, raiz)
 
-        arquivo.write("\t" + str(k) + "\t")
-        arquivo.write(str(xk) + "\t")
-        arquivo.write(str(fxk) + "\t")
-        arquivo.write(str(abs(xk - raiz)) + "\n")
-
-        # xrr == x (k - 2)
-        xrr = xr 
-        # xr == x (k-1)
-        xr = xk 
-        
-        # atualiza xk com base no xr e xrr
-        xk = (funcao(xr)*xrr - funcao(xrr)*xr)/(funcao(xr) - funcao(xrr))
-        mod = abs(xk - xr)
-    
-        k += 1
 
     arquivo.write("\t" + str(k) + "\t")
     arquivo.write(str(xk) + "\t")
-    arquivo.write(str(fxk) + "\t")
+    arquivo.write(str(funcao(xk)) + "\t")
+    arquivo.write(str(abs(xk - raiz)) + "\n")
+
+    #* INTERVALO [0,1]
+
+    k = 0 # começando com k igual a 0 e incrementando a cada iteração
+    xk = 0. # tendo o primeiro intervalo de [-1, 0] e estabelecendo xk = -1 e xr = 0
+    xr = 1. # xr, sendo r = k - 1 
+    xrr = None # xrr, sendo rr = r - 1 = k - 2
+    raiz = pow(3, 0.5)/3.
+    
+    arquivo.write("\n\nNo intervalo [0,1]\n")
+    arquivo.write("\tk\t     xk\t\t   f(xk)\t   erro\n")
+
+    # enquanto não for alcançada a precisão desejada e 
+    # k for menor que a maxiter o loop acontece
+    while(abs(xk - xr) > 0.000001 and k <= int(maxiter)):
+        k, xk, xr, xrr, arquivo = m_das_secantes(k, xk, xr, xrr, arquivo, raiz)
+
+
+    arquivo.write("\t" + str(k) + "\t")
+    arquivo.write(str(xk) + "\t")
+    arquivo.write(str(funcao(xk)) + "\t")
     arquivo.write(str(abs(xk - raiz)) + "\n")
 
     arquivo.close()
@@ -55,6 +61,27 @@ def funcao(x): # define a função que queremos aplicar o método de Newton, ten
     fx = 3*pow(x, 5) - 9*pow(x, 4) + 2*pow(x, 3) - 6*pow(x, 2) - x + 3
     return fx # retorna o resultado da função 
 
+def m_das_secantes(k, xk, xr, xrr, arquivo, raiz):
+    print(k)
+    fxk = funcao(xk)
+    fxr = funcao(xr)
+
+    arquivo.write("\t" + str(k) + "\t")
+    arquivo.write(str(xk) + "\t")
+    arquivo.write(str(fxk) + "\t")
+    arquivo.write(str(abs(xk - raiz)) + "\n")
+
+    # xrr == x (k - 2)
+    xrr = xr 
+    # xr == x (k-1)
+    xr = xk 
+    
+    # atualiza xk com base no xr e xrr
+    xk = (funcao(xr)*xrr - funcao(xrr)*xr)/(funcao(xr) - funcao(xrr))
+
+    k += 1
+
+    return k, xk, xr, xrr, arquivo
 
 if __name__ == "__main__": # definição da função main
     main()
